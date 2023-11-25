aa5575ff07d9c245c4090794d6dcd8845c69776e
Duhoon <eclip6@naver.com>
2023-11-25 14:53:08 +0900

    feat: add CSS for board page

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,17 +1,18 @@
 import { getPostList } from '@/api/post';
 import Link from 'next/link';
+import styles from './page.module.scss';
 
 export default async function Page(){
     const posts = await getPostList();
 
     return (
-        <div>
+        <div className={styles.board}>
             <h1>Posts</h1>
             <ul>
                 {posts.map(post=>(
                     <li key={post.slug}>
                         <Link href={`/post/${post.slug}`}>
-                            <h1>{post.title}</h1>
+                            <h2>{post.title}</h2>
                             <p>{post.published.toLocaleString()}</p>
                         </Link>
                     </li>
