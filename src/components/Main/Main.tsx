45b714d6de52fae8cbb55793e2dd380f6f929980
Duhoon <eclip6@naver.com>
2023-12-27 21:46:40 +0900

    feat: 카메라 이동 라이브러리 추가

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,14 +3,16 @@
 import * as Three from 'three';
 import { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+
 
 function Box(props: any){
     const ref = useRef();
 
     useFrame(()=>{
         if (ref){
-            ref.current.rotation.x += 0.01;
-            ref.current.rotation.y += 0.01;
+            ref.current.rotation.x += 0.005;
+            ref.current.rotation.y += 0.005;
         }
     })
     return (
@@ -20,16 +22,27 @@ function Box(props: any){
             scale={1}
         >
             <boxGeometry args={[1, 1, 1]} />
-            <meshBasicMaterial color='blue'/>
+            <meshStandardMaterial color='blue'/>
         </mesh>
     )
 }
 
 export function Main(){
     return (
-        <Canvas>
-            <ambientLight intensity={0.1} position={[0, 0, 0]}/>
-            <Box position={[0,0,0]}/>
+        <Canvas
+            linear={true}
+        >
+            <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+            <OrbitControls/>
+            <ambientLight intensity={5} position={[10, 0, 0]}/>
+            
+            <Box position={[0,0,5]}/>
+
+            <mesh position={[0, 0, 1]} rotation={[0, 0, 0]}>
+                <planeGeometry args={[10, 10]} />
+                <meshStandardMaterial color={'white'}/>
+            </mesh>
+            
         </Canvas>
     )
 }
