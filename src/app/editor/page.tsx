c978ab929942af090eed0b4f97dd268fb5c3060f
Duhoon <eclip6@naver.com>
2024-01-21 18:40:12 +0900

    feat: add react-md-editor

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,36 @@
+"use client"
+import { Navigation } from "@/components/Navigation";
+import { useState, useEffect } from "react";
+import MDEditor from '@uiw/react-md-editor';
+import styles from './page.module.scss';
+import { MainButton } from "@/components/Button";
+import { SecondaryButton } from "@/components/Button/Button";
+
+export default function Page(){
+    const [content, setContent] = useState(`
+---
+layout: 
+category: 
+published: 
+title: 
+---
+    `.replace(/\n/gm, ''));
+
+    useEffect(()=>{
+        console.log(content);
+    }, [content])
+
+    return (
+        <div>
+            <MDEditor
+                value={content}
+                onChange={(value)=>{ setContent(value || '') }}
+                height={'95vh'}
+            />
+            <div className={styles.editorBottom}>
+                <SecondaryButton>Cancel</SecondaryButton>
+                <MainButton>Save</MainButton>
+            </div>
+        </div>
+    )   
+}
