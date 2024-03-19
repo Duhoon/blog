4f2a101192a2c63ed1922eb96e3884d531130b83
Duhoon <eclip6@naver.com>
2024-03-19 20:44:25 +0900

    edit: Sync sitemap to post created

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,8 @@
+import { getPostListFromCloud } from "@/api/post";
 import { MetadataRoute } from "next";
 
-export default function sitemap(): MetadataRoute.Sitemap {
-    return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const sitemap: MetadataRoute.Sitemap = [
         {
             url: 'https://412ock.dev',
             lastModified: new Date(),
@@ -14,5 +15,33 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 0.8,
         },
-    ]
+        {
+            url: 'https://412ock.dev/blog',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+    ];
+
+    const categories = ['development', 'book', 'movie'];
+    
+    for (const category of categories) {
+        const posts = await getPostListFromCloud(category);
+        sitemap.push({
+            url: `https://412ock.dev/blog/${category}`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        })
+
+        const info = posts.map(post => ({
+            url: `https://412ock.dev/post/${category}/${post.slug}`,
+            lastModified: new Date(post.published),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        })) as MetadataRoute.Sitemap;
+        sitemap.push(...info);
+    }
+    
+    return sitemap;
 }
