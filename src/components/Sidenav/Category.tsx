2dddbd1d6b4046a071c9eeb7166260464280762b
Duhoon <eclip6@naver.com>
2024-08-11 15:28:17 +0900

    feat: apply dropdown UI to sub-category

--- src/components/Sidenav/Category.tsx
+++ src/components/Sidenav/Category.tsx
@@ -33,7 +33,14 @@ export function CategoryWithSub({text, children}: CategoryWithSubProps) {
 
   return (
       <li>
-          <h2 onClick={toggleOpen}>{text}</h2>
+          <div className='sub-category-header' onClick={toggleOpen}>
+            <h2>{text}</h2>
+            {
+              isOpen ?
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill='#fff' d="m12 6.586-8.707 8.707 1.414 1.414L12 9.414l7.293 7.293 1.414-1.414L12 6.586z"/></svg>
+              :<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path fill='#fff' d="M12 17.414 3.293 8.707l1.414-1.414L12 14.586l7.293-7.293 1.414 1.414L12 17.414z"/></svg> 
+            }            
+          </div>
           {             
               isOpen && 
               <ul className='sub-category'>
