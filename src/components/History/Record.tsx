9b9cd481ce75d7a55bc0aee6434c9ce5f585a680
Duhoon <eclip6@naver.com>
2024-12-09 04:07:55 +0900

    fix: about images use next Image with lazy loading

--- src/components/History/Record.tsx
+++ src/components/History/Record.tsx
@@ -13,9 +13,8 @@ export default function Record({record, isCur}: RecordProps){
   return (
     <article className='record-wrapper'>
       <div className='record-start'></div>
-      {/* <Image src={record.image} alt='image' width={IMAGE_WIDTH} height={IMAGE_HEIGHT}></Image> */}
       <div className='record-img-wrapper'>
-        <img className='record-img' src={record.image}/>
+        <Image src={record.image} alt='image' fill={true} loading={"lazy"}/>
       </div>
       <div className='record-body'>
         <h1 className='record-title'>{record.title}</h1>
