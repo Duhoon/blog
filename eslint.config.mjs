6b3ab20d96fde5493d076f680923a0b2ada543ae
Duhoon <eclip6@naver.com>
2024-12-31 02:33:58 +0900

    feature: set eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    ignores: ["eslint.config.mjs"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  pluginPrettier,
+);
