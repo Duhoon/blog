6acaa1504280d2c4d8d5d6c458099e59bdd70108
Duhoon <eclip6@naver.com>
2025-07-05 16:12:06 +0900

    chore: add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,20 @@
 import { ReactNode } from "react";
 import "./globals.css";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
-export default function RootLayout(children: ReactNode) {
-  return <body>{children}</body>
+export default function RootLayout({children}: {children: ReactNode}) {
+  return (
+    <html>
+      <body>
+      <SidebarProvider>
+        <AppSidebar/>
+        <main>
+          <SidebarTrigger/>
+          {children}
+        </main>
+      </SidebarProvider>
+      </body>
+    </html>
+  )
 }
