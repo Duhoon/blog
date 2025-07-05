f910e5b1f5a43ad305247b3a03ce3573c0c252d5
Duhoon <eclip6@naver.com>
2025-07-05 20:04:35 +0900

    feat: init internationalization with next-intl

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+import "../globals.css";
+import { NextIntlClientProvider, hasLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import { routing } from "@/i18n/routing";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/sidebar/app-sidebar";
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: ReactNode;
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html>
+      <body>
+        <NextIntlClientProvider>
+          <SidebarProvider>
+            <AppSidebar />
+            <main className={"w-screen"}>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
