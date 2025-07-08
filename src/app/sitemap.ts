905eee83ed52d5c8a76794f87e96400f2831e574
Duhoon <eclip6@naver.com>
2025-07-08 16:23:06 +0900

    fix: optimize sitemap generator

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,7 @@
-import { getPostList } from "@/api/posts.supabase";
+import { callGetPostList } from "@/api/posts.supabase";
 import { PostCategory } from "@/api/post";
 import { MetadataRoute } from "next";
+import path from "path";
 
 const baseUrl = "https://www.412ock.dev";
 export const locales = ["en-US", "ko"];
@@ -18,7 +19,8 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const categories = ["development", "book", "movie"] as PostCategory[];
   for (const category of categories) {
     for (const locale of locales) {
-      const { metadatas } = await getPostList(locale, category);
+      const filepath = path.join("posts", locale, category);
+      const { data } = await callGetPostList(filepath, 1, 1000);
       sitemap.push({
         url: `${baseUrl}/${locale}/list/${category}`,
         lastModified: new Date(),
@@ -26,9 +28,9 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         priority: 0.8,
       });
 
-      const info = metadatas.map((metadata) => ({
-        url: `${baseUrl}/${locale}/post/${category}/${metadata.slug}`,
-        lastModified: new Date(metadata.published),
+      const info = data.map((row) => ({
+        url: `${baseUrl}/${locale}/post/${category}/${row.name.replace(".md", "")}`,
+        lastModified: new Date(row.updated_at),
         changeFrequency: "never",
         priority: 0.5,
       })) as MetadataRoute.Sitemap;
