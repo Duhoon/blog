ae99d360026b9eda490e1e9d0ac7deeebe072d66
Duhoon <eclip6@naver.com>
2025-07-08 17:09:19 +0900

    fix: make temp text size responsively

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 export default async function Home() {
   return (
     <div className={"w-full h-screen flex justify-center items-center"}>
-      <h1 className={"text-[128px] font-bold"}>412ock Blog</h1>
+      <h1 className={"text-2xl lg:text-[128px] font-bold"}>412ock Blog</h1>
     </div>
   );
 }
